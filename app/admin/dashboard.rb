5b6aab86067ffc1c006ac8d5d2b3be7aeb0f2970
Vinchenso <chensok@gmail.com>
2017-07-16 21:47:34 +0200

    add sorting on id

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -2,12 +2,12 @@ ActiveAdmin.register_page "Dashboard" do
   content title: proc{ I18n.t("active_admin.dashboard") } do
 
     panel "Payments Schedule this month" do
-      table_for Property.all do
+      table_for Property.all.order('id desc') do
         column :name
-        column :tenant
-        column :agent {|p| p.agent unless p.agent.nil?}  
+        column :tenant 
+        column :agent {|p| p.agent unless p.agent.nil?}
         column :payments_recieved_this_month do |property|
-          link_to property.payments.this_month.sum(:amount), 
+          link_to property.payments.this_month.sum(:amount),
             admin_payments_path(q: { tenant_id_eq: property.tenant }, scope: :this_month)
         end
       end
@@ -16,8 +16,8 @@ ActiveAdmin.register_page "Dashboard" do
     panel "Total Recieved" do
       columns  do
         column { Payment.this_month.sum(:amount) }
-        end
       end
     end
-
   end
+
+end
