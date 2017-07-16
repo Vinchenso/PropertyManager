1859a52d3269d9e4410a1b637bdce0f5eb269c3f
Vinchenso <chensok@gmail.com>
2017-07-16 21:54:12 +0200

    syntax error

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -4,8 +4,10 @@ ActiveAdmin.register_page "Dashboard" do
     panel "Payments Schedule this month" do
       table_for Property.all.order('id desc') do
         column :name
-        column :tenant 
-        column :agent {|p| p.agent unless p.agent.nil?}
+        column :tenant
+        column :agent do |p|
+          p.agent unless p.agent.nil?
+        end
         column :payments_recieved_this_month do |property|
           link_to property.payments.this_month.sum(:amount),
             admin_payments_path(q: { tenant_id_eq: property.tenant }, scope: :this_month)
